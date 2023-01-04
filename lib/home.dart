78a370414963edee37739d7dd54a7a4f0f90c06d
Suraj Shah <surajshah_2001@yahoo.in>
2023-01-04 10:17:18 -0800

    Basics Revamped

--- lib/home.dart
+++ lib/home.dart
@@ -1,173 +1,145 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:rive/rive.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      backgroundColor: Colors.black,
+      body: Column(
         children: [
-          Stack(
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width * 0.45,
-                color: Colors.black,
-              ),
+              button("WORK", context),
+              const SizedBox(width: 60),
+              button("ABOUT", context),
+              const SizedBox(width: 100),
+              InkWell(onTap: () {}, child: Image.asset("assets/logo.png")),
+              const SizedBox(width: 100),
+              button("RESUME", context),
+              const SizedBox(width: 60),
+              button("CONTACT", context),
             ],
           ),
-          Positioned(
-            left: MediaQuery.of(context).size.width * 0.45,
-            child: Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width * 0.55,
-              color: Theme.of(context).secondaryHeaderColor,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 140),
-            child: Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                child: const RiveAnimation.asset('assets/portfolio1.riv')),
-          ),
-          Stack(
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Positioned(
-                left: 194,
-                top: MediaQuery.of(context).size.height / 2.47,
-                child: Text(
-                  "I'm \nSuraj Shah",
-                  style: TextStyle(
-                    foreground: Paint()
-                      ..style = PaintingStyle.stroke
-                      ..strokeWidth = 0.5
-                      ..color = Colors.white,
-                    fontSize: 98,
-                    fontFamily: 'gotham',
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 200,
-                top: MediaQuery.of(context).size.height / 2.5,
+              Container(
+                width: MediaQuery.of(context).size.width * 0.5,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text(
-                      "I'm \nSuraj Shah",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 98,
-                        fontFamily: 'gotham',
-                        fontWeight: FontWeight.w600,
-                      ),
+                    Stack(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 4.0, top: 2),
+                          child: Text(
+                            "Hey, I'm Suraj",
+                            style: TextStyle(
+                              foreground: Paint()
+                                ..style = PaintingStyle.stroke
+                                ..strokeWidth = 0.5
+                                ..color = Colors.white,
+                              fontSize: 80,
+                              fontFamily: 'gotham',
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        const Text(
+                          "Hey, I'm Suraj",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 80,
+                            fontFamily: 'gotham',
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 30),
-                    Container(
-                      height: 8,
-                      width: 160,
-                      color: Theme.of(context).primaryColor,
-                    )
+                    const SizedBox(height: 30),
+                    Text(
+                      "Graduate Student at CSULB and working upon front-end skills.",
+                      style: TextStyle(
+                          fontSize: 38,
+                          fontFamily: 'DMsans',
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    const SizedBox(height: 30),
+                    const Text(
+                      "There is lot to know about me, you can head on to my resume for brief summary of my background.",
+                      style: TextStyle(
+                          fontFamily: 'DMsans',
+                          fontSize: 18,
+                          letterSpacing: 0.5,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w300),
+                      textAlign: TextAlign.justify,
+                    ),
+                    const SizedBox(height: 60),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        padding: const EdgeInsets.fromLTRB(20, 16, 20, 16),
+                        textStyle: const TextStyle(
+                            fontFamily: 'DMsans',
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 1),
+                      ),
+                      child: const Text("Check out!"),
+                    ),
                   ],
                 ),
               ),
+              Column(
+                children: [
+                  Image.asset(
+                    "assets/illustration.png",
+                    scale: 1.3,
+                  ),
+                ],
+              )
             ],
-          ),
-          Positioned(
-            right: 0,
-            top: 20,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 80.0),
-                  child: Row(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black,
-                          padding: EdgeInsets.fromLTRB(20, 16, 20, 16),
-                          textStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 2),
-                        ),
-                        child: Text(
-                          "About",
-                        ),
-                      ),
-                      SizedBox(width: 40),
-                      ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.black,
-                            padding: EdgeInsets.fromLTRB(20, 16, 20, 16),
-                            textStyle: TextStyle(
-                                fontFamily: 'Montserrat',
-                                fontWeight: FontWeight.w400,
-                                letterSpacing: 2),
-                          ),
-                          child: Text("Projects")),
-                      SizedBox(width: 40),
-                      ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.black,
-                            padding: EdgeInsets.fromLTRB(20, 16, 20, 16),
-                            textStyle: TextStyle(
-                                fontFamily: 'Montserrat',
-                                fontWeight: FontWeight.w400,
-                                letterSpacing: 2),
-                          ),
-                          child: Text("Resume")),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 100.0, top: 200),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "— INTRODUCTION",
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.w300),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        "Front-End Developer \nwith pinch of knowledge in \nBack-End technology ",
-                        style: TextStyle(
-                            fontSize: 28,
-                            fontFamily: 'Myraid',
-                            color: Theme.of(context).primaryColor,
-                            fontWeight: FontWeight.w600),
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        "There is lot to know about me, you can head \non to my resume for brief summary of my \nbackground.",
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 18,
-                            letterSpacing: 0.5,
-                            fontWeight: FontWeight.w300),
-                        textAlign: TextAlign.justify,
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
           )
         ],
       ),
+    );
+  }
+
+  Widget button(String title, BuildContext context) {
+    bool val = false;
+    return ElevatedButton(
+      onPressed: () {},
+      onHover: (value) {
+        setState(() {
+          val = value;
+        });
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor:
+            val ? Theme.of(context).primaryColorLight : Colors.black,
+        padding: const EdgeInsets.fromLTRB(20, 16, 20, 16),
+        textStyle: const TextStyle(
+            fontFamily: 'DMsans',
+            fontWeight: FontWeight.w400,
+            letterSpacing: 2),
+      ),
+      child: Text(title),
     );
   }
 }
